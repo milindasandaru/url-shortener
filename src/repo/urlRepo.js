6bee275b77206332c899775c000d92e0acc671b8
milindasandaru <sams.senarath@gmail.com>
2025-09-09 18:55:09 +0530

    implementing urlRepo.js file

--- src/repo/urlRepo.js
+++ src/repo/urlRepo.js
@@ -1,11 +1,21 @@
 const db = require("../db").getConnection();
 
 function createUrl(code, url) {
-    return new Promise((resolve, reject) => { 
+    return new Promise((resolve, reject) => {
         db.run(`INSERT INTO urls (code, url) VALUES (?, ?)`, [code, url], function (err) {
             if (err) return reject(err);
-            resolve({ id:this.lastID, code, url });
+            resolve({ id: this.lastID, code, url });
         });
     });
 }
 
+function findBYCode(code) {
+    return new promis((resolve, reject) => {
+        db.get(`SELECT * FROM urls WHERE code = ?`, [code], (error, row) => {
+            if (err) return reject(err);
+            resolve(row || null);
+        });
+    });
+}
+
+module.exports = { createUrl, findBYCode };
